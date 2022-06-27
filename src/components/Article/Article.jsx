605a5e14c07e9bfd177c2dbd7abbd18429380faa
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-06-27 18:37:49 -0500

    bug fix

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,106 +1,88 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 // styles
 const Styledarticle = styled.article`
-border: 1px solid #000;
-border-bottom: none;
-padding: 10px;
-background: white;
-
-`
+  border: 1px solid #000;
+  border-bottom: none;
+  padding: 10px;
+  background: white;
+`;
 const Styledfigure = styled.figure`
-  margin:0;
+  margin: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
-    img{
-      width: 96%;
-      margin: 15px 0;
-      border-radius: 5px;
-    }
-`
+  img {
+    width: 96%;
+    margin: 15px 0;
+    border-radius: 5px;
+  }
+`;
 const StyledFigcaptionContainer = styled.div`
-text-align: center;
-    color: #979797;
-    figcaption{
-      margin-bottom: 3px;
-    }
-`
+  text-align: center;
+  color: #979797;
+  figcaption {
+    margin-bottom: 3px;
+  }
+`;
 const StyleDivcontainerTextInfoArtWork = styled.div`
-      width: 100%;
-    text-align: center;
-`
+  width: 100%;
+  text-align: center;
+`;
 const StyleDivTextInfoArtWork = styled.div`
   display: flex;
   justify-content: center;
   word-break: break-all;
-`
+`;
 const StyleDivTextLink = styled.div`
-word-break: break-all;
-    margin: 30px 40px;
-    line-height: 2;
-    a{
-      color: #0012ff;
+  word-break: break-all;
+  margin: 30px 40px;
+  line-height: 2;
+  a {
+    color: #0012ff;
     text-decoration: none;
-    }
-`
-export default function Article({images, loading}) {
-  const getImages =(item) => {
+  }
+`;
+export default function Article({ images }) {
+  const getImages = (item) => {
     try {
-      if(item.images.length === 0){
-       return(
-         <StyleDivcontainerTextInfoArtWork>
-           <StyleDivTextInfoArtWork>
-             <p>Culture: </p>
-             <p>{item.culture}</p>
-           </StyleDivTextInfoArtWork>
-           <StyleDivTextInfoArtWork>
-             <p>{item.people[0].role}: </p>
-             <p>{item.people[0].name}, {item.people[0].birthplace}</p>
-           </StyleDivTextInfoArtWork>
-           <StyleDivTextLink>
-             <p>VER IMAGEN EN:</p>
-               <a href={item.url} target="_blank" >{item.url} </a>
-              </StyleDivTextLink>
-         </StyleDivcontainerTextInfoArtWork>
-       )
-      }else{
-        if(item.images[0].baseimageurl){
-         return <img src={item.images[0].baseimageurl}></img>
-          }else{
-           return <img src={`${item.images[0].iiifbaseuri}/full/full/0/default.jpg`}> </img>
-          }
+      if (item.images.length === 0) {
+        return null;
+      } else {
+        if (item.images[0].baseimageurl) {
+          return <img src={item.images[0].baseimageurl}></img>;
+        } else {
+          return (
+            <img src={`${item.images[0].iiifbaseuri}/full/full/0/default.jpg`}>
+              {" "}
+            </img>
+          );
+        }
       }
     } catch (error) {
-      return null
+      return null;
     }
-
-  }
+  };
 
   return (
-  <>
-          {images.length > 1 &&(images.map((itemArray)=>(
-            itemArray.map((item) =>(
-             
-              <Styledarticle key={item.id} >
+    <>
+      {images.length > 1 &&
+        images.map((itemArray) =>
+          itemArray.map((item) => (
+            <Styledarticle key={item.id}>
               <h1>{item.title}</h1>
               <a href={item.url} target="_blank">
-              <Styledfigure>
-              {getImages(item)}
-                <StyledFigcaptionContainer>
-                <figcaption>{item.dated}</figcaption>
-                <figcaption>{item.classification}</figcaption>
-              </StyledFigcaptionContainer>
-
-            </Styledfigure>
-            </a>
-
+                <Styledfigure>
+                  {getImages(item)}
+                  <StyledFigcaptionContainer>
+                    <figcaption>{item.dated}</figcaption>
+                    <figcaption>{item.classification}</figcaption>
+                  </StyledFigcaptionContainer>
+                </Styledfigure>
+              </a>
             </Styledarticle>
-           
-
-            ))
-          )))}
-</>
-  
-  )
+          ))
+        )}
+    </>
+  );
 }
--- src/components/ArticleSearch/ArticleSearch.jsx
+++ src/components/ArticleSearch/ArticleSearch.jsx
@@ -1,109 +1,116 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 // styles
 const Styledarticle = styled.article`
-border: 1px solid #000;
-border-bottom: none;
-padding: 10px;
-background: white;
-.busqueda{
-  color: red;
-  font-size: 13px;
+  border: 1px solid #000;
+  border-bottom: none;
+  padding: 10px;
+  background: white;
+  .busqueda {
+    color: red;
+    font-size: 13px;
     text-align: end;
-}
-
-`
+  }
+`;
 const Styledfigure = styled.figure`
-  margin:0;
+  margin: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
-    img{
-      width: 96%;
-      margin: 15px 0;
-      border-radius: 5px;
-    }
-`
+  img {
+    width: 96%;
+    margin: 15px 0;
+    border-radius: 5px;
+  }
+`;
 const StyledFigcaptionContainer = styled.div`
-text-align: center;
-    color: #979797;
-    figcaption{
-      margin-bottom: 3px;
-    }
-`
+  text-align: center;
+  color: #979797;
+  figcaption {
+    margin-bottom: 3px;
+  }
+`;
 const StyleDivcontainerTextInfoArtWork = styled.div`
-      width: 100%;
-    text-align: center;
-`
+  width: 100%;
+  text-align: center;
+`;
 const StyleDivTextInfoArtWork = styled.div`
   display: flex;
   justify-content: center;
   word-break: break-all;
-`
+`;
 const StyleDivTextLink = styled.div`
-word-break: break-all;
-    margin: 30px 40px;
-    line-height: 2;
-    a{
-      color: #0012ff;
+  word-break: break-all;
+  margin: 30px 40px;
+  line-height: 2;
+  a {
+    color: #0012ff;
     text-decoration: none;
-    }
-`
-export default function Article({images, search}) {
-  const getImages =(item) => {
+  }
+`;
+export default function Article({ images, search }) {
+  const getImages = (item) => {
     try {
-      if(item.images.length === 0){
-       return(
-         <StyleDivcontainerTextInfoArtWork>
-           <StyleDivTextInfoArtWork>
-             <p>Culture: </p>
-             <p>{item.culture}</p>
-           </StyleDivTextInfoArtWork>
-           <StyleDivTextInfoArtWork>
-             <p>{item.people[0].role}: </p>
-             <p>{item.people[0].name}, {item.people[0].birthplace}</p>
-           </StyleDivTextInfoArtWork>
-           <StyleDivTextLink>
-             <p>VER IMAGEN EN:</p>
-               <a href={item.url} target="_blank" >{item.url} </a>
-              </StyleDivTextLink>
-         </StyleDivcontainerTextInfoArtWork>
-       )
-      }else{
-        if(item.images[0].baseimageurl){
-         return <img src={item.images[0].baseimageurl}></img>
-          }else{
-           return <img src={`${item.images[0].iiifbaseuri}/full/full/0/default.jpg`}> </img>
-          }
+      if (item.images.length === 0) {
+        return (
+          <StyleDivcontainerTextInfoArtWork>
+            <StyleDivTextInfoArtWork>
+              <p>Culture: </p>
+              <p>{item.culture}</p>
+            </StyleDivTextInfoArtWork>
+            <StyleDivTextInfoArtWork>
+              <p>{item.people[0].role}: </p>
+              <p>
+                {item.people[0].name}, {item.people[0].birthplace}
+              </p>
+            </StyleDivTextInfoArtWork>
+            <StyleDivTextLink>
+              <p>VER IMAGEN EN:</p>
+              <a href={item.url} target="_blank">
+                {item.url}{" "}
+              </a>
+            </StyleDivTextLink>
+          </StyleDivcontainerTextInfoArtWork>
+        );
+      } else {
+        if (item.images[0].baseimageurl) {
+          return <img src={item.images[0].baseimageurl}></img>;
+        } else {
+          return (
+            <img src={`${item.images[0].iiifbaseuri}/full/full/0/default.jpg`}>
+              {" "}
+            </img>
+          );
+        }
       }
     } catch (error) {
-      return null
+      return null;
     }
-
-  }
+  };
   return (
-  <>
-          {images.length > 0 ? (images.map((item)=>(
-              <Styledarticle key={item.id} >
-                <p className='busqueda'>{'-Busqueda-'}</p>
-                <h1>{item.title}</h1>
-              <a href={item.url} target="_blank">
-
+    <>
+      {images.length > 0 ? (
+        images.map((item) => (
+          <Styledarticle key={item.id}>
+            <p className="busqueda">{"-Busqueda-"}</p>
+            <h1>{item.title}</h1>
+            <a href={item.url} target="_blank">
               <Styledfigure>
-              {getImages(item)}
+                {getImages(item)}
                 <StyledFigcaptionContainer>
-                <figcaption>{item.dated}</figcaption>
-                <figcaption>{item.classification}</figcaption>
-              </StyledFigcaptionContainer>
-            </Styledfigure>
+                  <figcaption>{item.dated}</figcaption>
+                  <figcaption>{item.classification}</figcaption>
+                </StyledFigcaptionContainer>
+              </Styledfigure>
             </a>
-
-           
-            
-            </Styledarticle>
-          ))):(<h1 className='not-found'>No se Encontraron resultados para "{search}" :(</h1>)}
-</>
-  
-  )
+          </Styledarticle>
+        ))
+      ) : (
+        <h1 className="not-found">
+          No se Encontraron resultados para "{search}" :(
+        </h1>
+      )}
+    </>
+  );
 }
